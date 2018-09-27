e16b642b246c99335de238b01cc218d6a709bf52
delmargaret <ri.tysik@mail.ru>
2018-09-27 21:58:06 +0300

    класс Projects для добавления проектов и их сроков

--- App/Program.cs
+++ App/Program.cs
@@ -19,6 +19,18 @@ namespace App
             Console.WriteLine();
             roles.ShowRoles();
             #endregion
+
+            Console.WriteLine();
+
+            #region Projects
+            Projects projects = new Projects();
+            projects.AddProject(projects.CreateProject("project1", DateTimeOffset.Now, new DateTimeOffset(2018, 10, 21, 10, 15, 35, new TimeSpan(3, 0, 0))));
+            projects.AddProject(projects.CreateProject("project2", DateTimeOffset.Now, new DateTimeOffset(2018, 12, 5, 13, 30, 0, new TimeSpan(3, 0, 0))));
+            projects.ShowProjects();
+            projects.RemoveProjectByName("project2");
+            Console.WriteLine();
+            projects.ShowProjects();
+            #endregion
         }
     }
 }
---
+++ App/Projects.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    class Projects
+    {
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public DateTimeOffset ProjectStartDate { get; set; }
+        public DateTimeOffset ProjectEndDate { get; set; }
+        public List<Projects> ProjectList = new List<Projects>();
+
+        public Projects() { }
+
+        public Projects(int id, string name, DateTimeOffset startdate, DateTimeOffset enddate)
+        {
+            this.ProjectId = id;
+            this.ProjectName = name;
+            this.ProjectStartDate = startdate;
+            this.ProjectEndDate = enddate;
+        }
+
+        public Projects CreateProject(string projectname, DateTimeOffset startdate, DateTimeOffset enddate)
+        {
+            ProjectId++; //------------?
+            ProjectName = projectname;
+            ProjectStartDate = startdate;
+            if (enddate < DateTimeOffset.Now)
+            {
+                Console.WriteLine("incorrect end date"); //------------?
+                return null;
+            }
+            if (enddate < startdate)
+            {
+                Console.WriteLine("incorrect end date");
+                return null;
+            }
+            ProjectEndDate = enddate;
+            return new Projects(ProjectId, ProjectName, ProjectStartDate, ProjectEndDate);
+        }
+
+        public List<Projects> AddProject(Projects project)
+        {
+            if (project == null) //------------?
+            {
+                ProjectId--;
+                return ProjectList;
+            }
+            ProjectList.Add(project);
+            return ProjectList;
+        }
+
+        public List<Projects> RemoveProjectByName(string name)
+        {
+            Projects result = new Projects();
+            foreach (var project in ProjectList)
+            {
+                if (project.ProjectName == name)
+                {
+                    result = project;
+                    break;
+                }
+            }
+            ProjectList.Remove(result);
+            return ProjectList;
+        }
+
+        public List<Projects> RemoveProjectById(int id)
+        {
+            ProjectList.RemoveAt(--id);
+            return ProjectList;
+
+        }
+
+        public void ShowProjects()
+        {
+            foreach (var project in ProjectList)
+            {
+                Console.WriteLine("id: " + project.ProjectId + " name: " + project.ProjectName
+                + "\nstart date: " + project.ProjectStartDate.ToString() + " end date: " + project.ProjectEndDate.ToString());
+            }
+        }
+    }
+}
