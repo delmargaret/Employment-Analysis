459729d0c43d71c8e6da668f677ce9081990c20c
delmargaret <ri.tysik@mail.ru>
2018-09-28 00:03:46 +0300

    класс Employee для описания сотрудников

---
+++ App/Employees.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    class Employees
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeSurname { get; set; }
+        public string EmployeePatronymic { get; set; }
+        public string Email { get; set; } //регулярные выражения?
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public List<Employees> EmployeeList = new List<Employees>();
+
+        public Employees() { }
+
+        public Employees(int id, string name, string surname, string patronymic, string email, string password)
+        {
+            this.EmployeeId = id;
+            this.EmployeeName = name;
+            this.EmployeeSurname = surname;
+            this.EmployeePatronymic = patronymic;
+            this.Email = email;
+            this.Password = password;
+            this.PhoneNumber = "";
+        }
+
+        public Employees(int id, string name, string surname, string patronymic, string email, string password, string phonenumber)
+        {
+            this.EmployeeId = id;
+            this.EmployeeName = name;
+            this.EmployeeSurname = surname;
+            this.EmployeePatronymic = patronymic;
+            this.Email = email;
+            this.PhoneNumber = phonenumber;
+            this.Password = password;
+        }
+
+        public Employees RegisterEmployee(string name, string surname, string patronymic, string email, string password)
+        {
+            EmployeeId++;
+            EmployeeName = name;
+            EmployeeSurname = surname;
+            EmployeePatronymic = patronymic;
+            Email = email;
+            Password = password;
+            PhoneNumber = "";
+            return new Employees(EmployeeId, EmployeeName, EmployeeSurname, EmployeePatronymic, Email, Password, PhoneNumber);
+        }
+
+        public List<Employees> AddEmployee(Employees employee)
+        {
+            EmployeeList.Add(employee);
+            return EmployeeList;
+        }
+
+        public Employees GetEmployeeByName(string name)
+        {
+            int id = -1;
+            foreach(var employee in EmployeeList)
+            {
+                if (employee.EmployeeName == name)
+                {
+                    id = employee.EmployeeId;
+                }
+            }
+            if (id == -1)
+            {
+                return null;
+            }
+            return EmployeeList[--id];
+        }
+
+        //public void AddPhoneNumber(int employeeid, string phonenumber)
+        //{
+        //    foreach (var employee in EmployeeList)
+        //    {
+        //        if (employee.EmployeeId == employeeid)
+        //        {
+        //            employee.PhoneNumber = phonenumber;
+        //        }
+        //    }
+        //}
+
+        public void ChangeEmployeeName(int employeeid, string newname)
+        {
+            foreach (var employee in EmployeeList)
+            {
+                if (employee.EmployeeId == employeeid)
+                {
+                    employee.EmployeeName = newname;
+                }
+            }
+        }
+
+        public void ChangeEmployeeSurname(int employeeid, string newsurname)
+        {
+            foreach (var employee in EmployeeList)
+            {
+                if (employee.EmployeeId == employeeid)
+                {
+                    employee.EmployeeSurname = newsurname;
+                }
+            }
+        }
+
+        public void ChangeEmployeePatronymic(int employeeid, string newpatronymic)
+        {
+            foreach (var employee in EmployeeList)
+            {
+                if (employee.EmployeeId == employeeid)
+                {
+                    employee.EmployeePatronymic = newpatronymic;
+                }
+            }
+        }
+
+        public void ChangeEmail(int employeeid, string newemail)
+        {
+            foreach (var employee in EmployeeList)
+            {
+                if (employee.EmployeeId == employeeid)
+                {
+                    employee.Email = newemail;
+                }
+            }
+        }
+
+        public void ChangePassword(int employeeid, string newpassword)
+        {
+            foreach (var employee in EmployeeList)
+            {
+                if (employee.EmployeeId == employeeid)
+                {
+                    employee.Password = newpassword;
+                }
+            }
+        }
+
+        public void ShowEmployees()
+        {
+            foreach(var employee in EmployeeList)
+            {
+                if (PhoneNumber == "")
+                {
+                    Console.WriteLine("id: " + employee.EmployeeId + " name: " + employee.EmployeeName
+                    + " e-mail: " + employee.Email + " password: " + employee.Password);
+                }
+                else
+                {
+                    Console.WriteLine("id: " + employee.EmployeeId + " name: " + employee.EmployeeName
+                    + " e-mail: " + employee.Email + " password: " + employee.Password + " phone number: " + employee.PhoneNumber);
+                }
+            }
+        }
+    }
+}
--- App/Program.cs
+++ App/Program.cs
@@ -10,26 +10,41 @@ namespace App
     {
         static void Main(string[] args)
         {
-            #region Roles
-            Roles roles = new Roles();
-            roles.AddRole(roles.CreateRole("manager"));
-            roles.AddRole(roles.CreateRole("designer"));
-            roles.ShowRoles();
-            roles.RemoveRoleByName("designer");
-            Console.WriteLine();
-            roles.ShowRoles();
-            #endregion
+            //#region Roles
+            //Roles roles = new Roles();
+            //roles.AddRole(roles.CreateRole("manager"));
+            //roles.AddRole(roles.CreateRole("designer"));
+            //roles.ShowRoles();
+            //roles.RemoveRoleByName("designer");
+            //Console.WriteLine();
+            //roles.ShowRoles();
+            //#endregion
 
-            Console.WriteLine();
+            //Console.WriteLine();
 
-            #region Projects
-            Projects projects = new Projects();
-            projects.AddProject(projects.CreateProject("project1", DateTimeOffset.Now, new DateTimeOffset(2018, 10, 21, 10, 15, 35, new TimeSpan(3, 0, 0))));
-            projects.AddProject(projects.CreateProject("project2", DateTimeOffset.Now, new DateTimeOffset(2018, 12, 5, 13, 30, 0, new TimeSpan(3, 0, 0))));
-            projects.ShowProjects();
-            projects.RemoveProjectByName("project2");
-            Console.WriteLine();
-            projects.ShowProjects();
+            //#region Projects
+            //Projects projects = new Projects();
+            //projects.AddProject(projects.CreateProject("project1", DateTimeOffset.Now, new DateTimeOffset(2018, 10, 21, 10, 15, 35, new TimeSpan(3, 0, 0))));
+            //projects.AddProject(projects.CreateProject("project2", DateTimeOffset.Now, new DateTimeOffset(2018, 12, 5, 13, 30, 0, new TimeSpan(3, 0, 0))));
+            //projects.ShowProjects();
+            //projects.RemoveProjectByName("project2");
+            //Console.WriteLine();
+            //projects.ShowProjects();
+            //#endregion
+
+            //Console.WriteLine();
+
+            #region
+            Employees employees = new Employees();
+            employees.AddEmployee(employees.RegisterEmployee("Margo", "Delikatnaya", "Mihailovna", "hello@mail.ru", "123456"));
+            employees.AddEmployee(employees.RegisterEmployee("Vasya", "Ivanov", "Olegovich", "world@mail.ru", "9876543"));
+            employees.ShowEmployees();
+            var result = employees.GetEmployeeByName("Margo");
+            Console.WriteLine(result.Email);
+            employees.ChangeEmployeeName(1, "Olya");
+            employees.ShowEmployees();
+            employees.AddPhoneNumber(1, "+1254893");
+            employees.ShowEmployees();
             #endregion
         }
     }
